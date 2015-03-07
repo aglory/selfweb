f76e1f886021f85ff23fd66e1f1903ead3561e11
aglory <Fireman_duck@hotamil.com>
2015-03-07 17:30:39 +0800

    联系方式修改

--- cgi/footer.php
+++ cgi/footer.php
@@ -1,3 +1,10 @@
 <footer class="wrap">
 	<hr />
+	<dl>
+		<dt>联系方式：</dt>
+		<dd>地址：四川省资中县西街9号楼14号（5层）</dd>
+		<dd>电话：13568021185</dd>
+		<dd>QQ:69550298</dd>
+		<dd>微信:xie185868</dd>
+	</dl>
 </footer>
