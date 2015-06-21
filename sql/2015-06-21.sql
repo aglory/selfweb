5ffeee7ab6b5a2237c6cb33dfc3fccc94f6deffd
aglory <Fireman_duck@hotamil.com>
2015-06-21 11:03:09 +0800

    2

--- sql/2015-06-21.sql
+++ sql/2015-06-21.sql
@@ -1,28 +1,25 @@
-create table tbUserInfo(
+
+create table tbClassPropertyKeyInfo(
 	id int auto_increment primary key,
-    `name` varchar(50) not null comment '名称',
-	`password` varchar(50) not null comment '密码',
-    privilege int not null default 0 comment '权限',
+    classid int not null default 0 comment '所属班级',
+	`name` varchar(100) not null comment '属性名',
+    targetlevel int not null default 0 comment '指定信息重要程度',
+    displaytype int not null default 0 comment '展示方式 1：默认，2：无序，3有序',
+	`order` int not null default 0 comment '顺序',
 	datecreate timestamp not null default 0 comment '建立时间',
 	datemodify timestamp not null default 0 comment '修改时间',
 	status int not null default 0 comment '状态(1:正常,2:停用)'
-)engine='MyISAM'  comment '用户';
+) engine='MyISAM'  comment '班级附加信息名称';
 
-insert into tbUserInfo(`name`,`password`,datecreate,datemodify,status)values('admin',md5('123456'),now(),now(),1);
 
-create table tbSessionIfo(
-	userid int not null primary key comment '用户编号',
-	clientid varchar(36) not null unique comment '客户端唯一编号',
-	ip varchar(20) not null unique comment '使用ip地址',
-	datecreate timestamp not null default 0 comment '建立时间',
-	datemodify timestamp not null default 0 comment '修改时间'
-)engine='Memory' comment '会话';
-
-create table tbLoginHistoryInfo(
+create table tbClassPropertyValueInfo(
 	id int auto_increment primary key,
-    `name` varchar(50) not null comment '用户名称',
-	`password` varchar(50) not null comment '用户密码',
-	ip varchar(20) not null comment '使用ip地址',
-	issuccess int not  null default 0 comment '登录结果0失败，1成功',
-	datecreate timestamp not null default 0 comment '建立时间'
-)engine Archive comment '登录记录';
+    classid int not null default 0 comment '所属班级',
+    keyid int not null default 0 comment '所属附加 班级信息名称',
+	`value` varchar(400) not null comment '属性名',
+    targetlevel int not null default 0 comment '指定信息重要程度',
+	`order` int not null default 0 comment '顺序',
+	datecreate timestamp not null default 0 comment '建立时间',
+	datemodify timestamp not null default 0 comment '修改时间',
+	status int not null default 0 comment '状态(1:正常,2:停用)'
+) engine='MyISAM'  comment '班级附加信息值';
