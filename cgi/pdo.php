fb187d6d038658474a0ce79378bb7211ac45bb92
aglory <Fireman_duck@hotamil.com>
2015-06-29 22:51:24 +0800

    d

--- cgi/pdo.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(!defined('Execute')){ exit();}
-$pdomysql = new PDO('mysql:host=fireman.gotoftp3.com;dbname=fireman','fireman','a747258621900z');
-//$pdomysql = new PDO('mysql:host=.;dbname=fireman','root','123456');
-?>
